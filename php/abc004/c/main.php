43670c46efd35f6b9958f106ad9a8f43658e22f3
melank <masaki.vr@gmail.com>
2026-01-23 01:01:29 +0900

    abc004/cの解答を追加

--- php/abc004/c/main.php
+++ php/abc004/c/main.php
@@ -9,18 +9,6 @@ function getInt(): int {
     return (int) getLine();
 }
 
-function getIntArray(): array {
-    return array_map('intval', explode(' ', getLine()));
-}
-
-function getLines(int $n): array {
-    $lines = [];
-    for ($i = 0; $i < $n; $i++) {
-        $lines[] = getLine();
-    }
-    return $lines;
-}
-
 // Output
 function printResult(int|float|string $result): void {
     echo $result . "\n";
@@ -28,7 +16,19 @@ function printResult(int|float|string $result): void {
 
 // Main
 function solve(): void {
-    // Write your solution here
+    $n = getInt();
+
+    // 30回で元に戻るので、N mod 30 回だけ操作
+    $n = $n % 30;
+
+    $cards = [1, 2, 3, 4, 5, 6];
+
+    for ($i = 0; $i < $n; $i++) {
+        $pos = $i % 5;  // 0-indexed: 位置 pos と pos+1 を入れ替え
+        [$cards[$pos], $cards[$pos + 1]] = [$cards[$pos + 1], $cards[$pos]];
+    }
+
+    printResult(implode('', $cards));
 }
 
 solve();
