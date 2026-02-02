babbead7dce8bffdfade1a136d3f3b9f184edf75
melank <masaki.vr@gmail.com>
2026-02-02 14:41:06 +0900

    関数型に実装し直す

--- php/practice/99_Bottles _of_Beer_on_the_Wall/main.php
+++ php/practice/99_Bottles _of_Beer_on_the_Wall/main.php
@@ -1,29 +1,15 @@
 <?php
 
-// 回答の末尾には改行を付けなければいけない
-function printResult(int|float|string $result): void
-{
-    echo (string) $result . "\n";
-}
-
-/**
- * 99 Bottles of Beer on the Wall の歌詞を生成する
- *
- * @return string 完全な歌詞
- */
 function solution(): string {
-    for ($i = 99; $i >= 2; $i--) {
-        printResult("{$i} bottles of beer on the wall, {$i} bottles of beer.");
-        printResult("Take one down and pass it around, {$i} bottles of beer on the wall.");
-        printResult("");
-    }
-    printResult("1 bottle of beer on the wall, 1 bottle of beer.");
-    printResult("Take one down and pass it around, 1 bottle of beer on the wall.");
-    printResult("");
-    printResult("No more bottles of beer on the wall, no more bottles of beer.");
-    printResult("Go to the store and buy some more, 99 bottles of beer on the wall.");
-
-    return '';
+    $bottle = fn($n) => match(true) {
+        $n === 0 => "no more bottles",
+        $n === 1 => "1 bottle",
+        default  => "{$n} bottles"
+    };
+    
+    $verse = fn($n) => $n > 0
+        ? "{$bottle($n)} of beer on the wall, {$bottle($n)} of beer.\nTake one down and pass it around, {$bottle($n-1)} of beer on the wall."
+        : "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.";
+    
+    return implode("\n\n", array_map($verse, range(99, 0)));
 }
-
-solution();
